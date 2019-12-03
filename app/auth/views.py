f84f282055a478b59ffc05d69a07b2ab438b9729
NzauM <mercywaenu16@gmail.com>
2019-12-03 18:56:49 +0300

    add view function to logout user

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,7 @@ from . import auth
 from ..models import User
 from .forms import RegistrationForm,LoginForm
 from .. import db
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 
 
 @auth.route('/register',methods = ['GET','POST'])
@@ -31,4 +31,8 @@ def login():
         title = "Log into E-MAY"
         return render_template('auth/login.html',login_form = login_form,title = title)
 
-
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
