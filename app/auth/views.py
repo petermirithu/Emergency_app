f97f5bac7837dc6747125aa7d91126e5c736f68f
NzauM <mercywaenu16@gmail.com>
2019-12-03 18:44:07 +0300

    Add a login view function

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,10 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,flash,request
 from . import auth
 from ..models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm,LoginForm
 from .. import db
+from flask_login import login_user
+
 
 @auth.route('/register',methods = ['GET','POST'])
 def register():
@@ -14,4 +16,19 @@ def register():
         return redirect(url_for('auth.login'))
         title = "New User"
     return render_template('auth/register.html',registration_form = form)
+
+@auth.route('/login',methods = ['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email =login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+        title = "Log into E-MAY"
+        return render_template('auth/login.html',login_form = login_form,title = title)
+
 
