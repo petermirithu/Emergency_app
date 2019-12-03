a85ab24d702f769e9e5a0ba3f4a947db850f6cee
NzauM <mercywaenu16@gmail.com>
2019-12-03 16:46:20 +0300

    Add load user function to the user class

--- app/models.py
+++ app/models.py
@@ -1,10 +1,14 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
     pass_secure = db.Column(db.String(255))
 
 
@@ -19,6 +23,9 @@ class User(db.Model):
     def verify_password(self,password):
         return check_password_hash(self.pass_secure.password)
 
+    @login_manager.user_loader
+    def load_user(user_id):
+         return User.query.get(int(user_id))
 
     def __repr__(self):
         return f'User {self.username}'
