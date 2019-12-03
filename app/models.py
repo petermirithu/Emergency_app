331128dd8c3681d114c9c7498830cd1e0d878098
NzauM <mercywaenu16@gmail.com>
2019-12-03 16:16:29 +0300

    Add functions for verifying passwords

--- app/models.py
+++ app/models.py
@@ -1,6 +1,24 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+
+
+    @property
+    def password(self):
+        raise AttributeError('Password attribute cannot be read')
+
+    @password.setter
+    def password(self.password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure.password)
+
+
+    def __repr__(self):
+        return f'User {self.username}'
