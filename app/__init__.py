d7b79afd5e41149d5a20028f1d34c34e998fe7cb
AmosMwangi <amosmwangi101@gmail.com>
2019-12-03 11:54:57 +0300

    initialized the db

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # ....
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
