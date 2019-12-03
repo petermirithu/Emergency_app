4c619ad33ee8e0ccf8d33692430013bd3b9f8a6c
NzauM <mercywaenu16@gmail.com>
2019-12-03 16:47:53 +0300

    Add a sign up view function

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,17 @@
-from flask import render_template
+from flask import render_template,redirect,url_for
 from . import auth
+from ..models import User
+from .forms import RegistrationForm
+from .. import db
 
-@auth.route('/login')
-def login():
-    return render_template('auth/login.html')
+@auth.route('/register',methods = ['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data,username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New User"
+    return render_template('auth/register.html',registration_form = form)
+
