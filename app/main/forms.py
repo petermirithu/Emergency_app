44adba049c7ef264e4fd29db7f3ca969f8d7d071
petermirithu <pyra_m.k@yahoo.com>
2019-12-04 14:43:26 +0300

    remove one submit field

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,8 +8,7 @@ class EmergencyForm(FlaskForm):
     category = SelectField(u'Select category', choices = [('Accidents','Accident'),('Floods','Floods'),('Earthquakes','Earthquakes'),('Flu','flu'),('Fire','Fire'),('Landslide','Landslide'),('PowerOutage','Power Outage'),('Terrorism','Terrorism'),('Wildfire','Wildfire')], validators = [Required()])
     description = StringField('write a brief description of the emergency (optional)')
     location = StringField('Write your location', validators = [Required()])
-    submit = SubmitField('Submit')
-    submit = SubmitField('Submit')
+    submit = SubmitField('Submit')    
 
 class ConvoForm(FlaskForm):
     '''
