11b7c4307c3a6929fac6163988fa4d41d1612c65
NzauM <mercywaenu16@gmail.com>
2019-12-04 09:16:18 +0300

    Add destination for uploaded photos

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
         
-    # UPLOADED_PHOTOS_DEST ='app/static/photos'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     # MAIL_SERVER = 'smtp.googlemail.com'
     # MAIL_PORT = 587
     # MAIL_USE_TLS = True
