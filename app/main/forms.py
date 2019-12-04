75587e0bfaa665fcfe3e2d96c5ce0006bf8e595f
dan <dan@localhost.localdomain>
2019-12-04 08:27:10 +0300

    add a conversation form and template

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,7 +4,15 @@ from wtforms.validators import Required
 from wtforms import ValidationError
 
 class EmergencyForm(FlaskForm):
-    category = SelectField(u'Select category', choices = [('Accident','Accident'),('Floods','Floods'),('Earthquakes','Earthquakes'),('Flu','flu'),('Fire','Fire'),('Landslide','Landslide'),('Power Outage','Power Outage'),('Terrorism','Terrorism'),('Wildfire','Wildfire')], validators = [Required()])
+    category = SelectField(u'Select category', choices = [('Accident','Accident'),('Floods','Floods'),('Earthquakes','Earthquakes'),('Flu','flu'),('Fire','Fire'),('Landslide','Landslide'),('PowerOutage','Power Outage'),('Terrorism','Terrorism'),('Wildfire','Wildfire')], validators = [Required()])
     description = StringField('write a brief description of the emergency (optional)')
     location = StringField('Write your location', validators = [Required()])
     submit = SubmitField('Submit')
+    submit = SubmitField('Submit')
+
+class ConvoForm(FlaskForm):
+    '''
+    class definig convo form
+    '''
+    convo=SelectField('Enter your text',validators=[Required()])     
+    submit=SubmitField('post')   
