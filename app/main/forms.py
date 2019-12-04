4f120ff983cea8dfa7cb08baad84f21847a52c4b
petermirithu <pyra_m.k@yahoo.com>
2019-12-04 11:18:23 +0300

    add a chatbox  form

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import Required
 from wtforms import ValidationError
 
 class EmergencyForm(FlaskForm):
-    category = SelectField(u'Select category', choices = [('Accident','Accident'),('Floods','Floods'),('Earthquakes','Earthquakes'),('Flu','flu'),('Fire','Fire'),('Landslide','Landslide'),('PowerOutage','Power Outage'),('Terrorism','Terrorism'),('Wildfire','Wildfire')], validators = [Required()])
+    category = SelectField(u'Select category', choices = [('Accidents','Accident'),('Floods','Floods'),('Earthquakes','Earthquakes'),('Flu','flu'),('Fire','Fire'),('Landslide','Landslide'),('PowerOutage','Power Outage'),('Terrorism','Terrorism'),('Wildfire','Wildfire')], validators = [Required()])
     description = StringField('write a brief description of the emergency (optional)')
     submit = SubmitField('Submit')
 
@@ -14,3 +14,11 @@ class ConvoForm(FlaskForm):
     '''
     convo=SelectField('Enter your text',validators=[Required()])     
     submit=SubmitField('post')   
+
+class chatboxForm(FlaskForm):
+    '''
+    class defining the chat form
+    '''
+    chatbox=TextAreaField('Enter you text')
+    submit=SubmitField('Submit')
+    
