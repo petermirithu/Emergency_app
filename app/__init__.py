d8d0ef6e0827e180839696b83c96a76cb387a6b6
NzauM <mercywaenu16@gmail.com>
2019-12-04 09:16:55 +0300

    Configure upload sets

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from config import config_options
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -29,6 +32,9 @@ def create_app(config_name):
     from .request import configure_request
     configure_request(app)
     login_manager.init_app(app)
+
+    # Configuring uploadset
+    configure_uploads(app,photos)
 
 # register auth blueprint
     from .auth import auth as auth_blueprint
