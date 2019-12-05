cc33686a9d793fb325c6692cc4f050504c70ad19
petermirithu <pyra_m.k@yahoo.com>
2019-12-05 15:54:05 +0300

    add location class

--- app/models.py
+++ app/models.py
@@ -180,6 +180,11 @@ class Article:
         self.title = title
 
 
+class Location:
+
+    def __init__(self,latitude,longitude):
+        self.latitude = latitude
+        self.longitude = longitude
 
 
 
